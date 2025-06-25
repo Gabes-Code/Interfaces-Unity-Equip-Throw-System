6b3d9c130bc91461520d3e666e8b4e022024ac36
Gabriel Huseas <gabescode@protonmail.com>
2025-06-24 18:24:06 -0600

    Add files via upload

---
+++ Scripts.cs
@@ -0,0 +1,73 @@
+﻿
+
+//INTERFACES
+
+// IEquippable.cs
+public interface IEquippable
+{
+    void Equip();
+}
+
+
+//IThrowable.cs
+public interface IThrowable
+{
+    void Throw();
+}
+
+//Item.cs
+using UnityEngine;
+
+public class Item: MonoBehaviour
+{
+    public string itemName;
+}
+
+//BASE ITEM CLASS
+
+//Item.cs
+using UnityEngine;
+
+public class Item: MonoBehaviour
+{
+    public string itemName;
+}
+
+//EXAMPLE ITEMS
+// Sword.cs
+using UnityEngine;
+
+public class Sword : Item, IEquipable
+{
+    public void Equip()
+    {
+        Debug.Log($"You equipped the {itemName}!");
+    }
+}
+
+// Rock.cs
+using UnityEngine;
+
+public class Rock : Item, IThrowable
+{
+    public void Throw()
+    {
+        Debug.Log($"You threw the {itemName}!");
+    }
+}
+
+// Spear.cs
+using UnityEngine;
+
+public class Spear : Item, IEquipable, IThrowable
+{
+    public void Equip()
+    {
+        Debug.Log($"You equipped the {itemName}!");
+    }
+
+    public void Throw()
+    {
+        Debug.Log($"You threw the {itemName}!");
+    }
+}
